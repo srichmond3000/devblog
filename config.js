0db64739953a8c662f84df8dabc0dbc8b1655797
srichmond3000 <srichmond3000@googlemail.com>
2018-12-28 10:45:47 +0000

    Fixing Linkedin link

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    linkedin: 'www.linkedin.com/in/steve-richmond-8732ab6'
+    linkedin: 'https://linkedin.com/in/steve-richmond-8732ab6'
   },
   pathPrefix: '/devblog',
   siteUrl: 'https://github.com/srichmond3000/devblog',
